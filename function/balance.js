c80eaf4043a25fac5ed624a0e3cc7ea802bb7018
alexMarian704 <alex.boss.123441@gmail.com>
2021-12-16 15:08:47 +0200

    profile

---
+++ function/balance.js
@@ -0,0 +1,12 @@
+import { Moralis } from "moralis";
+
+let userBalance;
+
+export const getBalance = async (userETHaddress) => {
+    const balances = await Moralis.Web3API.account.getNativeBalance({
+        address: userETHaddress,
+    });
+    userBalance = (balances.balance / 1000000000000000000).toFixed(5);
+    
+    return userBalance;
+};
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Nav from "../components/Nav";
 import Head from "next/head";
 import { useMoralis } from "react-moralis";
@@ -6,9 +6,16 @@ import Reject from "../components/Reject";
 import ConfigAccount from "../components/ConfigAccount";
 import { Moralis } from "moralis";
 import Image from "next/image";
+import style from "../styles/Profile.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { getBalance } from "../function/balance";
 
 export default function profile() {
   const { isAuthenticated, user, setUserData } = useMoralis();
+  const fileRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [balance , setBalance] = useState(0);
 
   if (!isAuthenticated) {
     return <Reject />;
@@ -25,13 +32,20 @@ export default function profile() {
     const type = e.target.files[0].type.replace("image/", "");
     const name = `profile.${type}`;
     const profileFile = new Moralis.File(name, file);
+    setLoading(true);
     await profileFile.saveIPFS();
     console.log(profileFile.ipfs());
     console.log(profileFile.hash());
     setUserData({
       profilePhoto: profileFile.ipfs(),
     });
+    setLoading(false);
   };
+
+  const userETHaddress = user.get("ethAddress");
+  getBalance(userETHaddress).then((result)=>{
+      setBalance(result)
+  })
 
   return (
     <div>
@@ -40,18 +54,48 @@ export default function profile() {
       </Head>
       <Nav />
       <div className="marginDiv"></div>
-      <h1>profile</h1>
-      <div className="imgMeta">
-        {user.get("profilePhoto") !== undefined && (
-          <Image
-            src={user.get("profilePhoto")}
-            alt="profilePhoto"
-            width="90%" height="90%" layout="responsive"
-            objectFit="contain"
-          />
-        )}
+      <div className={style.main}>
+        <div className={style.imgProfile}>
+          {user.get("profilePhoto") !== undefined && (
+            <Image
+              src={user.get("profilePhoto")}
+              alt="profilePhoto"
+              width="90%"
+              height="90%"
+              layout="responsive"
+              objectFit="contain"
+              className={style.img}
+            />
+          )}
+          {loading && (
+            <div className={style.loadingContainer}>
+              <div className={style.loader}></div>
+            </div>
+          )}
+          <button
+            onClick={() => {
+              fileRef.current.click();
+            }}
+            className={style.change}
+          >
+            <FontAwesomeIcon icon={faCamera} color="white" />
+          </button>
+        </div>
+        <input
+          type="file"
+          onChange={changePhoto}
+          ref={fileRef}
+          style={{
+            display: "none",
+          }}
+        />
+        <div className={style.dataUser}>
+          <h2 className={style.address}>Username: {user.get("username")}</h2>
+          <h2 className={style.address}>Tag: @{user.get("userTag")}</h2>
+          <h2 className={style.address}>Address: {userETHaddress}</h2>
+          <h2 className={style.address}>ETH Balance: {balance}</h2>
+        </div>
       </div>
-        <input type="file" onChange={changePhoto} />
     </div>
   );
 }
